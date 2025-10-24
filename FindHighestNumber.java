e9a05fb98524a11f44ca04174ef3be48312174aa
codewithkhushboo <khushboo.git@outook.com>
2025-10-24 18:51:17 +0530

    Added math.max

--- FindHighestNumber.java
+++ FindHighestNumber.java
@@ -9,8 +9,23 @@ public class FindHighestNumber {
         int number3=(number1>number2)?number1:number2;
         System.out.println("Highest number between "+number1+" and "+number2+" is "+number3);
         sc.close();
+        
+        
+        
+        /*System.out.print("Enter the first number (num1): ");
+        int num1 = sc.nextInt();
+        
+        System.out.print("Enter the second number (num2): ");
+        int num2 = sc.nextInt();
+        
+        // Store the highest number in a new variable called 'highest'
+        int highest = Math.max(num1, num2);
+        
+        System.out.println("\nThe highest number is: " + highest);
+        
+        */
+    }
+}
 
         
-    }
     
-}

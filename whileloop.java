408acc43e2ad6be17bbd809d0f8b27cfdcc77e0c
codewithkhushboo <khushboo.git@outook.com>
2025-10-25 11:51:38 +0530

    WhileLoop

---
+++ whileloop.java
@@ -0,0 +1,11 @@
+public class whileloop {
+    public static void main(String[] args) {
+        int i;
+        i=0;
+        while(i<=5){
+            System.out.print(i);
+            i++;
+        }
+        //System.out.print(i);
+    }
+}

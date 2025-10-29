fedfab4ce987064b43eef0be74969a6d392ca0e0
codewithkhushboo <khushboo.git@outook.com>
2025-10-29 09:12:05 +0530

    beautifying code

--- pattern/BitwisePrint.java
+++ pattern/BitwisePrint.java
@@ -3,12 +3,14 @@ public class BitwisePrint {
         for (int i=1;i<=5;i++){
             for(int j=1;j<=i;j++){
                 int sum=i+j;
-                if(sum%2==0){
+                if(sum%2==0)
+                {
                     System.out.print("1");
                 }
                     else
                     {
-                        System.out.print("0");}
+                        System.out.print("0");
+                    }
 
                 }
                 System.out.println();

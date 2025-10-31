0b1be05736cf5e5465dd5c613f95167e40bfeec3
codewithkhushboo <khushboo.git@outook.com>
2025-10-31 13:29:35 +0530

    ButterFlyPattern

---
+++ pattern/ButterFlyPattern.java
@@ -0,0 +1,31 @@
+public class ButterFlyPattern{
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int spaces=1;spaces<=2*(5-i); spaces++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=5;i>=1;i--)
+        {
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int spaces=1;spaces<=2*(5-i);spaces++){
+                System.out.print(" ");}
+                for(int j=1;j<=i;j++){
+                    System.out.print("*");
+                }
+            
+            System.out.println();
+        }
+    }
+}

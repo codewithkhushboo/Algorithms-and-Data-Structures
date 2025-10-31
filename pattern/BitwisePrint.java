cdda9cf230c377f3fffd65141ec5b9d3ae5e1917
codewithkhushboo <khushboo.git@outook.com>
2025-10-31 12:08:45 +0530

    bitwise

--- pattern/BitwisePrint.java
+++ pattern/BitwisePrint.java
@@ -5,11 +5,11 @@ public class BitwisePrint {
                 int sum=i+j;
                 if(sum%2==0)
                 {
-                    System.out.print("1");
+                    System.out.print("1 ");
                 }
                     else
                     {
-                        System.out.print("0");
+                        System.out.print("0 ");
                     }
 
                 }

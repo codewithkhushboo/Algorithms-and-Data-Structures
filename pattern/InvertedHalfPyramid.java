93052d918b8f0b1ca634232186802d189e7b402b
codewithkhushboo <khushboo.git@outook.com>
2025-11-02 22:23:26 +0530

    InvertedHalfPyramid

---
+++ pattern/InvertedHalfPyramid.java
@@ -0,0 +1,13 @@
+public class InvertedHalfPyramid {
+    public static void main(String[] args) {
+        int number=1;
+        for(int i=4;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(number);
+            }
+            System.out.println();
+            number++;
+        }
+    }
+    
+}

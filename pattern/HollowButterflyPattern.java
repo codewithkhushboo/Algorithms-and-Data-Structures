13f4181f8bb5273784221054db9ad4be2556ff53
codewithkhushboo <khushboo.git@outook.com>
2025-11-02 20:20:44 +0530

    HollowButterflyPattern

---
+++ pattern/HollowButterflyPattern.java
@@ -0,0 +1,55 @@
+public class HollowButterflyPattern {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                if(j==1|| j==i){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            for(int spaces=1;spaces<=2*(5-i);spaces++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                if(j==1||j==i){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        for(int i=5;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                if(j==1||j==i){
+                    System.out.print("*");
+                }
+                else
+                System.out.print(" ");
+            }
+            for(int spaces=1;spaces<=2*(5-i);spaces++)
+            System.out.print(" ");
+            for(int j=1;j<=i;j++){
+                if(j==1||j==i){
+                System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+            
+        }
+        System.out.println();
+    }
+
+    
+}
+
+}
+
+  
+
+    
+

26ac3bd829d7d2fa5d5a5be3ad114559c765674a
codewithkhushboo <khushboo.git@outook.com>
2025-11-10 02:15:56 +0530

    pattern

---
+++ pattern/OneTwoThree.java
@@ -0,0 +1,28 @@
+public class OneTwoThree {
+    public static void main(String[] args) {
+        for(int i=1;i<=3;i++){
+            for(int space=i; space>1;space--){
+            System.out.print(" ");
+            }
+            for(int j=i;j<=3;j++)
+            System.out.print(j);
+        System.out.println();
+        }
+
+    }
+    
+}
+//public class OneTwoThree {
+//     public static void main(String[] args) {
+//         for(int i=1;i<=3;i++){
+//             for(int space=i; space>1;space--){
+//             System.out.print(" ");
+//             }
+//             for(int j=i;j<=3;j++)
+//             System.out.print(j+" ");
+//         System.out.println();
+//         }
+
+//     }
+    
+// }
---
+++ pattern/OnetwoOne.java
@@ -0,0 +1,22 @@
+//   1
+//  121
+// 12321
+
+public class OnetwoOne {
+    public static void main(String[] args) {
+        for(int i=1;i<=3;i++){
+            for(int space=1;space<=3-i;space++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j);
+            }
+            for(int j=i-1;j>=1;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    
+}
